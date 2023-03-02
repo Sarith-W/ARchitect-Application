a6cc30d65b3afdf049bc697a734fd0192aa09e69
Sarith Wijesundera <smw8079@gmail.com>
2023-03-02 12:12:39 +0530

    Update home.dart

--- Version 2.0/ARchitect_App/lib/home.dart
+++ Version 2.0/ARchitect_App/lib/home.dart
@@ -64,7 +64,7 @@ class Home extends StatelessWidget {
                         context,
                         sizedBoxHeight,
                         'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-                        'EDIT.'),
+                        'EDIT'),
                     const SizedBox(height: 15.0),
                     SizedBox(
                       width: double.infinity,
