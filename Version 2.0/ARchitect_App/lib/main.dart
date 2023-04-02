58121156cb86eee6fba79d80c635d13990d85dae
Sarith Wijesundera <smw8079@gmail.com>
2023-04-02 16:34:40 +0530

    Delete Version 2.0/ARchitect_App directory

--- Version 2.0/ARchitect_App/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:architect_app/welcomePage.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: WelcomePage()));
-}
