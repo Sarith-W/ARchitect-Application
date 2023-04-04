effd2a3c3acfe02ba0137dffe94e18cab0f11a19
Ganindu Pushpakumara <117251065+Poorna0426@users.noreply.github.com>
2023-04-04 22:52:33 +0530

    Update help_screen.dart

--- lib/HelpScreen/help_screen.dart
+++ lib/HelpScreen/help_screen.dart
@@ -152,9 +152,251 @@ class HelpScreen extends StatelessWidget {
                                     fontWeight: FontWeight.w400
                                 ),
                               ),
-                              SizedBox(height: 20),
+                              SizedBox(height: 10),
                               Text(
                                 'Step 3: Select View in AR.',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          width: 320,
+                          decoration: const BoxDecoration(
+                              color: Color(0xFF2d3436),
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.grey,
+                                    blurRadius: 5.0,
+                                    spreadRadius: 1.0,
+                                    offset: Offset(0.0, 3)
+                                )
+                              ]
+                          ),
+                          child: Column(
+                            children: const [
+                              SizedBox(height: 20),
+                              Text(
+                                'Method 3:',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                'Step 1: Go to the Home',
+                                textAlign: TextAlign.left, // add this line to align the text to the left
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Step 2: Select a product type (Sofa or Table)',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Step 3: Select a product.',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Step 4: Select View in AR',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 5.0,
+                              spreadRadius: 1.0,
+                              offset: Offset(0.0, 3)
+                          )
+                        ]
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        const Text(
+                          'How to rate the furniture arrangement?',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          width: 320,
+                          decoration: const BoxDecoration(
+                              color: Color(0xFF2d3436),
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.grey,
+                                    blurRadius: 5.0,
+                                    spreadRadius: 1.0,
+                                    offset: Offset(0.0, 3)
+                                )
+                              ]
+                          ),
+                          child: Column(
+                            children: const [
+                              SizedBox(height: 20),
+                              Text(
+                                'Step 1: In the AR view, click on the up arrow at the bottom of the screen',
+                                textAlign: TextAlign.left, // add this line to align the text to the left
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Step 2: Click on the “Rate the Furniture Arrangement” button',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Step 3: A preview will be displayed. Then click anywhere outside the preview window.',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Step 4: The result will be shown',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 5.0,
+                              spreadRadius: 1.0,
+                              offset: Offset(0.0, 3)
+                          )
+                        ]
+                    ),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        const Text(
+                          'How to check colour suggestions?',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Container(
+                          width: 320,
+                          decoration: const BoxDecoration(
+                              color: Color(0xFF2d3436),
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.grey,
+                                    blurRadius: 5.0,
+                                    spreadRadius: 1.0,
+                                    offset: Offset(0.0, 3)
+                                )
+                              ]
+                          ),
+                          child: Column(
+                            children: const [
+                              SizedBox(height: 20),
+                              Text(
+                                'Step 1: Go to the ‘Home’ or the sidebar.',
+                                textAlign: TextAlign.left, // add this line to align the text to the left
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Step 2: Click “Check Colour Suggestions',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Step 3: Take an image',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Step 4: The recommended colour will be displayed.',
                                 style: TextStyle(
                                     color: Colors.white,
                                     fontSize: 16,
