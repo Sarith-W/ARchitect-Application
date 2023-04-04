dee5cd8318bae90e608123f830e764009843fb07
Ganindu Pushpakumara <117251065+Poorna0426@users.noreply.github.com>
2023-04-04 22:37:50 +0530

    add method three

--- lib/HelpScreen/help_screen.dart
+++ lib/HelpScreen/help_screen.dart
@@ -121,6 +121,49 @@ class HelpScreen extends StatelessWidget {
                                 )
                               ]
                           ),
+                          child: Column(
+                            children: const [
+                              SizedBox(height: 20),
+                              Text(
+                                'Method 2:',
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                'Step 1: Go to the Home',
+                                textAlign: TextAlign.left, // add this line to align the text to the left
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Step 2: Click the three lines in the top \nleft corner',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                'Step 3: Select View in AR.',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                            ],
+                          ),
                         ),
                         const SizedBox(height: 20),
                       ],
